1b69d3c3f376ca03f708599ccf13c2af166ba85c
Ada <xandrewsteam@gmail.com>
2026-01-29 22:20:13 +0100

    Add reset button with prompt, function (WIP)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const resetBtn = document.getElementById('resetBtn');
+    resetBtn.addEventListener('click', function() {
+        newSize = prompt("Enter new grid size (max 100):");        if (newSize === null) return;
+        let size= parseInt (newSize);
+    });
+
 const container = document.querySelector('.container');
 function createGrid(numSquare) {
-    for (let i = 0; i <numSquare; i++) {
+    for (let i = 0; i < numSquare; i++) {
         const square = document.createElement('div'); 
         square.classList.add('square');
         container.appendChild(square);
@@ -10,4 +16,6 @@ function createGrid(numSquare) {
     }
 }
 
-createGrid(256);
+
+
+createGrid(resetBtn);
